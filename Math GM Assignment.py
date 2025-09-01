1994543a3464c88be254a4b80a36ce3a9a1906b8
Govinda Mohabir <govindamohabir@gmail.com>
2025-08-31 23:04:26 -0400

    Add files via upload

---
+++ Math GM Assignment.py
@@ -0,0 +1,24 @@
+result = 5 * 6
+print(result)
+
+result = 8 / 3
+print(result)
+
+result = 8 // 3
+print(result)
+
+result = 8 % 3
+print(result)
+
+num1 = 4200000
+num2 = 23090
+result = num1 + num2
+print(result)
+
+num1 = 4200000
+num2 = 7633
+result = num1 - num2
+print(result)
+
+print("Name: Govinda Mohabir"
+      " COP1000 8/31/25")
